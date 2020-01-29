9af5f8e48f56ba8e93e15bf10b5fa2174e9c3042
lfernandez55 <lfernandez@weber.edu>
2020-01-29 16:45:23 -0700

    Commit

--- countWords.py
+++ countWords.py
@@ -14,9 +14,19 @@ for line in fhand:
             counts[word] = 1
         else:
             counts[word] += 1
-
+print("----------Displaying the newly created dictionary with unsorted word counts)----------")
 print(counts)
+print("----------Sorted by value (but only displaying value)----------")
+print(sorted(counts.values()))
+print("==========Sorted by value (but also displaying key)=============")
+# sort the words by frequency by printing out a list of tuples
+# the number in the bracket determines whether to sort of key (0)
+# or value (1)
+list_of_tuples = sorted(counts.items(), key=lambda item: item[1])
+for x in list_of_tuples:
+    print(x)
 
+print("+++++++++++++Finding the highest word (with duplicates)+++++++++++++++++++")
 # find one of the words with the highest counts
 highest_value=0
 highest_key = ''
@@ -26,10 +36,4 @@ for key, value in counts.items():
         highest_value = value
         highest_key = key
 
-print("highest key: ", highest_key, "highest value: ", highest_value)
-
-# sort the words by frequency by printing out a list of tuples
-list_of_tuples = sorted(counts.items(), key=lambda item: item[1])
-
-for x in list_of_tuples:
-    print(x)
+print("highest key: '", highest_key, "'  highest value: ", highest_value)
