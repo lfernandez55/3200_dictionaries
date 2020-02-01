408fc39487780b2b14c258e2029c0a23641c7876
lfernandez55 <lfernandez@weber.edu>
2020-01-31 17:11:10 -0700

    Fixing and Optimizing Dungeon Demo

--- dungeon_demo.py
+++ dungeon_demo.py
@@ -1,16 +1,14 @@
 top_room = {}
 middle_room = {}
 bottom_room = {}
-
-top_room = {'adjacent_rooms':[middle_room]}
-middle_room = {'adjacent_rooms':[top_room,bottom_room]}
-bottom_room = {'adjacent_rooms':[middle_room]}
+top_room['adjacent_rooms']=[middle_room]
+middle_room['adjacent_rooms']=[top_room, bottom_room]
+bottom_room['adjacent_rooms']=[middle_room]
 
 rooms = {'top_room':top_room, 'middle_room': middle_room, 'bottom_room': bottom_room}
 
-current_room = 'middle_room'
+current_room = 'top_room'
 request = ""
-print(rooms[current_room])
 
 while request != 'quit':
     print("U r in the ", current_room)
